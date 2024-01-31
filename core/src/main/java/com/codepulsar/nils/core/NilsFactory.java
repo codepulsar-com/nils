40c89bde0a1dde4c60b9c7210bbcb872c87eccf8
Marco Huber <kontakt@marcohuber.de>
2024-01-31 09:52:30 +0100

    Move logic from NilsFactory to NilsFactoryImpl

--- core/src/main/java/com/codepulsar/nils/core/NilsFactory.java
+++ core/src/main/java/com/codepulsar/nils/core/NilsFactory.java
@@ -1,40 +1,18 @@
 package com.codepulsar.nils.core;
 
-import static com.codepulsar.nils.core.util.ParameterCheck.NILS_CONFIG;
-
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.codepulsar.nils.core.adapter.AdapterConfig;
-import com.codepulsar.nils.core.adapter.AdapterFactory;
-import com.codepulsar.nils.core.error.ErrorType;
-import com.codepulsar.nils.core.error.NilsException;
 import com.codepulsar.nils.core.handler.ClassPrefixResolver;
-import com.codepulsar.nils.core.impl.NLSImpl;
-import com.codepulsar.nils.core.util.ParameterCheck;
+import com.codepulsar.nils.core.impl.NilsFactoryImpl;
 /** Factory for getting access to the provided NLS. A requested NLS object is cached. */
-public class NilsFactory {
-  private static final Logger LOG = LoggerFactory.getLogger(NilsFactory.class);
-  private final NilsConfig config;
-  private AdapterFactory<?> adapterFactory;
-
-  private final Map<Locale, NLS> translationCache = new HashMap<>();
-
-  private NilsFactory(NilsConfig config) {
-    this.config = ParameterCheck.notNull(config, "config", NILS_CONFIG);
-  }
+public interface NilsFactory {
   /**
    * Get the NLS object for the default <code>Locale</code>.
    *
    * @return The NLS object for the default <code>Locale</code>.
    */
-  public NLS nls() {
-    return nls(Locale.getDefault());
-  }
+  NLS nls();
   /**
    * Get the NLS object for a language tag.
    *
@@ -42,28 +20,14 @@ public class NilsFactory {
    * @return The NLS object for the language tag.
    * @see Locale#forLanguageTag(String)
    */
-  public NLS nls(String lang) {
-    ParameterCheck.notNullEmptyOrBlank(lang, "lang");
-    lang = lang.replace("_", "-");
-    return nls(Locale.forLanguageTag(lang));
-  }
+  NLS nls(String lang);
   /**
    * Get the NLS object for a specific <code>Locale</code>.
    *
    * @param locale The <code>Locale</code>.
    * @return The NLS object for the <code>Locale</code>.
    */
-  public NLS nls(Locale locale) {
-
-    NLS result =
-        translationCache.computeIfAbsent(
-            locale,
-            (l) -> {
-              return createImpl(locale, config);
-            });
-    return result;
-  }
-
+  NLS nls(Locale locale);
   /**
    * Get the NLS object for the default <code>Locale</code> and a specific context.
    *
@@ -72,11 +36,7 @@ public class NilsFactory {
    * @param context The context key.
    * @return The NLS object for the default <code>Locale</code>.
    */
-  public NLS nlsWithContext(String context) {
-    ParameterCheck.notNullEmptyOrBlank(context, "context");
-    return nls().context(context);
-  }
-
+  NLS nlsWithContext(String context);
   /**
    * Get the NLS object for a language tag and a specific context.
    *
@@ -87,10 +47,7 @@ public class NilsFactory {
    * @return The NLS object for the language tag.
    * @see Locale#forLanguageTag(String)
    */
-  public NLS nlsWithContext(String lang, String context) {
-    ParameterCheck.notNullEmptyOrBlank(context, "context");
-    return nls(lang).context(context);
-  }
+  NLS nlsWithContext(String lang, String context);
 
   /**
    * Get the NLS object for a specific <code>Locale</code> and a specific context.
@@ -101,10 +58,7 @@ public class NilsFactory {
    * @param context The context key.
    * @return The NLS object for the <code>Locale</code>.
    */
-  public NLS nlsWithContext(Locale locale, String context) {
-    ParameterCheck.notNullEmptyOrBlank(context, "context");
-    return nls(locale).context(context);
-  }
+  NLS nlsWithContext(Locale locale, String context);
 
   /**
    * Get the NLS object for the default <code>Locale</code> and a specific context based on a <code>
@@ -118,10 +72,7 @@ public class NilsFactory {
    * @param context The <code>Class</code> for the context key.
    * @return The NLS object for the default <code>Locale</code>.
    */
-  public NLS nlsWithContext(Class<?> context) {
-    ParameterCheck.notNull(context, "context");
-    return nls().context(context);
-  }
+  NLS nlsWithContext(Class<?> context);
 
   /**
    * Get the NLS object for a language tag and a specific context based on a <code>
@@ -139,10 +90,7 @@ public class NilsFactory {
    * @return The NLS object for the language tag.
    * @see Locale#forLanguageTag(String)
    */
-  public NLS nlsWithContext(String lang, Class<?> context) {
-    ParameterCheck.notNull(context, "context");
-    return nls(lang).context(context);
-  }
+  NLS nlsWithContext(String lang, Class<?> context);
 
   /**
    * Get the NLS object for a language tag and a specific context based on a <code>
@@ -158,28 +106,7 @@ public class NilsFactory {
    * @return The NLS object for the language tag.
    * @see Locale#forLanguageTag(String)
    */
-  public NLS nlsWithContext(Locale locale, Class<?> context) {
-    ParameterCheck.notNull(context, "context");
-    return nls(locale).context(context);
-  }
-
-  private NLSImpl createImpl(Locale locale, NilsConfig config) {
-    return new NLSImpl(
-        getAdapterFactory().create(config.getAdapterConfig(), locale), config, locale);
-  }
-
-  private AdapterFactory<?> getAdapterFactory() {
-    if (adapterFactory == null) {
-      try {
-        this.adapterFactory =
-            config.getAdapterConfig().getFactoryClass().getConstructor().newInstance();
-      } catch (ReflectiveOperationException | IllegalArgumentException | SecurityException e) {
-        LOG.error("Could not create AdapterFactory. Reason {}", e.getMessage(), e);
-        throw new NilsException(ErrorType.ADAPTER_ERROR, "Could not create AdapterFactory.", e);
-      }
-    }
-    return adapterFactory;
-  }
+  NLS nlsWithContext(Locale locale, Class<?> context);
 
   /**
    * Initialize the factory for an adapter using its {@link AdapterConfig}.
@@ -188,7 +115,7 @@ public class NilsFactory {
    * @return The create factory.
    */
   public static NilsFactory init(AdapterConfig adapterConfig) {
-    return new NilsFactory(NilsConfig.init(adapterConfig));
+    return new NilsFactoryImpl(NilsConfig.init(adapterConfig));
   }
 
   /**
@@ -198,6 +125,6 @@ public class NilsFactory {
    * @return The create factory.
    */
   public static NilsFactory init(NilsConfig config) {
-    return new NilsFactory(config);
+    return new NilsFactoryImpl(config);
   }
 }
---
+++ core/src/main/java/com/codepulsar/nils/core/impl/NilsFactoryImpl.java
@@ -0,0 +1,108 @@
+package com.codepulsar.nils.core.impl;
+
+import static com.codepulsar.nils.core.util.ParameterCheck.NILS_CONFIG;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.codepulsar.nils.core.NLS;
+import com.codepulsar.nils.core.NilsConfig;
+import com.codepulsar.nils.core.NilsFactory;
+import com.codepulsar.nils.core.adapter.AdapterFactory;
+import com.codepulsar.nils.core.error.ErrorType;
+import com.codepulsar.nils.core.error.NilsException;
+import com.codepulsar.nils.core.util.ParameterCheck;
+/** Factory for getting access to the provided NLS. A requested NLS object is cached. */
+public class NilsFactoryImpl implements NilsFactory {
+  private static final Logger LOG = LoggerFactory.getLogger(NilsFactoryImpl.class);
+  private final NilsConfig config;
+  private AdapterFactory<?> adapterFactory;
+
+  private final Map<Locale, NLS> translationCache = new HashMap<>();
+
+  public NilsFactoryImpl(NilsConfig config) {
+    this.config = ParameterCheck.notNull(config, "config", NILS_CONFIG);
+  }
+
+  @Override
+  public NLS nls() {
+    return nls(Locale.getDefault());
+  }
+
+  @Override
+  public NLS nls(String lang) {
+    ParameterCheck.notNullEmptyOrBlank(lang, "lang");
+    lang = lang.replace("_", "-");
+    return nls(Locale.forLanguageTag(lang));
+  }
+
+  @Override
+  public NLS nls(Locale locale) {
+
+    NLS result =
+        translationCache.computeIfAbsent(
+            locale,
+            (l) -> {
+              return createImpl(locale, config);
+            });
+    return result;
+  }
+
+  @Override
+  public NLS nlsWithContext(String context) {
+    ParameterCheck.notNullEmptyOrBlank(context, "context");
+    return nls().context(context);
+  }
+
+  @Override
+  public NLS nlsWithContext(String lang, String context) {
+    ParameterCheck.notNullEmptyOrBlank(context, "context");
+    return nls(lang).context(context);
+  }
+
+  @Override
+  public NLS nlsWithContext(Locale locale, String context) {
+    ParameterCheck.notNullEmptyOrBlank(context, "context");
+    return nls(locale).context(context);
+  }
+
+  @Override
+  public NLS nlsWithContext(Class<?> context) {
+    ParameterCheck.notNull(context, "context");
+    return nls().context(context);
+  }
+
+  @Override
+  public NLS nlsWithContext(String lang, Class<?> context) {
+    ParameterCheck.notNull(context, "context");
+    return nls(lang).context(context);
+  }
+
+  @Override
+  public NLS nlsWithContext(Locale locale, Class<?> context) {
+    ParameterCheck.notNull(context, "context");
+    return nls(locale).context(context);
+  }
+
+  private NLSImpl createImpl(Locale locale, NilsConfig config) {
+    return new NLSImpl(
+        getAdapterFactory().create(config.getAdapterConfig(), locale), config, locale);
+  }
+
+  private AdapterFactory<?> getAdapterFactory() {
+    if (adapterFactory == null) {
+      try {
+        this.adapterFactory =
+            config.getAdapterConfig().getFactoryClass().getConstructor().newInstance();
+      } catch (ReflectiveOperationException | IllegalArgumentException | SecurityException e) {
+        LOG.error("Could not create AdapterFactory. Reason {}", e.getMessage(), e);
+        throw new NilsException(ErrorType.ADAPTER_ERROR, "Could not create AdapterFactory.", e);
+      }
+    }
+    return adapterFactory;
+  }
+}

e62e9271fc48ca84d225759218a5ae09ebe486ab
Marco Huber <kontakt@marcohuber.de>
2024-02-21 10:17:07 +0100

    Remove example files from tests

--- core/src/test/java/com/codepulsar/nils/core/examples/Address.java
+++
@@ -1,6 +0,0 @@
-package com.codepulsar.nils.core.examples;
-
-import com.codepulsar.nils.api.NLS;
-
-/** Class for demonstration of the {@linkplain NLS#get(Class, String)} method. */
-public class Address {}
